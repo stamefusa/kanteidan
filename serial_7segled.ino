8cdf108c7f2c66d5d4ba317ef6f6c9c7c951b299
Shintaro Tamefusa <tameshin@gmail.com>
2020-08-29 23:05:40 +0900

    -を渡したときは非表示ではなくランダム数字の表示に変更

--- serial_7segled.ino
+++ serial_7segled.ino
@@ -36,26 +36,26 @@ int state = STATE_STOP;
 String key = "";
 
 void numPrint(int num) {
-  for (int j=0; j<=6; j++) {
-    digitalWrite(j+6, num_array[num][j]);
+  for (int j = 0; j <= 6; j++) {
+    digitalWrite(j + 6, num_array[num][j]);
   }
 }
 
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(10); // SerialでのString受信のタイムアウト設定（ms）
-  
+
   pinMode(digit_1, OUTPUT); // 光る桁
   pinMode(digit_10, OUTPUT); // 光る桁
   pinMode(digit_100, OUTPUT); // 光る桁
   pinMode(digit_1000, OUTPUT); // 光る桁
-  for (int i=6; i<=12; i++) {
+  for (int i = 6; i <= 12; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
 void loop() {
-  if (Serial.available() > 0){
+  if (Serial.available() > 0) {
     key = Serial.readStringUntil(';');
     key.trim();
     Serial.print("receive : ");
@@ -68,7 +68,7 @@ void loop() {
     } else {
       if (key.length() == 4) {
         state = STATE_NUMBER;
-        for (int k=0; k<4; k++) {
+        for (int k = 0; k < 4; k++) {
           digit_chars[k] = key.charAt(k);
         }
       }
@@ -95,75 +95,45 @@ void dispInit() {
 }
 
 // 指定の数字を桁ごとに表示
-// 非表示は"-"で表現する
+// "-"はランダムの数値を表示する
 void dispNumbers(String digit_1_char, String digit_10_char, String digit_100_char, String digit_1000_char) {
   // 1の桁
   digitalWrite(digit_10, 0);
   digitalWrite(digit_100, 0);
   digitalWrite(digit_1000, 0);
-  if (digit_1_char == "-") {
-    digitalWrite(digit_1, 0);
-  } else {
-    digitalWrite(digit_1, 1);
-  }
-  int digit_1_num = digit_1_char.toInt();
-  if (digit_1_num < 0 || digit_1_num > 9) {
-    numPrint(0);
-  } else {
-    numPrint(digit_1_num);
-  }
+  digitalWrite(digit_1, 1);
+  digit_1_num = convertNum(digit_1_char, digit_1_num, count);
+  numPrint(digit_1_num);
   delay(del);
-  
+
   // 10の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_100, 0);
   digitalWrite(digit_1000, 0);
-  if (digit_10_char == "-") {
-    digitalWrite(digit_10, 0);
-  } else {
-    digitalWrite(digit_10, 1);
-  }
-  int digit_10_num = digit_10_char.toInt();
-  if (digit_10_num < 0 || digit_10_num > 9) {
-    numPrint(0);
-  } else {
-    numPrint(digit_10_num);
-  }
+  digitalWrite(digit_10, 1);
+  digit_10_num = convertNum(digit_10_char, digit_10_num, count);
+  numPrint(digit_10_num);
   delay(del);
-  
+
   // 100の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_10, 0);
   digitalWrite(digit_1000, 0);
-  if (digit_100_char == "-") {
-    digitalWrite(digit_100, 0);
-  } else {
-    digitalWrite(digit_100, 1);
-  }
-  int digit_100_num = digit_100_char.toInt();
-  if (digit_100_num < 0 || digit_100_num > 9) {
-    numPrint(0);
-  } else {
-    numPrint(digit_100_num);
-  }
+  digitalWrite(digit_100, 1);
+  digit_100_num = convertNum(digit_100_char, digit_100_num, count);
+  numPrint(digit_100_num);
   delay(del);
-  
+
   // 1000の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_10, 0);
   digitalWrite(digit_100, 0);
-  if (digit_1000_char == "-") {
-    digitalWrite(digit_1000, 0);
-  } else {
-    digitalWrite(digit_1000, 1);
-  }
-  int digit_1000_num = digit_1000_char.toInt();
-  if (digit_1000_num < 0 || digit_1000_num > 9) {
-    numPrint(0);
-  } else {
-    numPrint(digit_1000_num);
-  }
+  digitalWrite(digit_1000, 1);
+  digit_1000_num = convertNum(digit_1000_char, digit_1000_num, count);
+  numPrint(digit_1000_num);
   delay(del);
+
+  count = (count + 1) % 5;
 }
 
 // 全LEDを0～9の中からランダムで表示
@@ -174,7 +144,7 @@ void dispRandom() {
     digit_100_num = random(9);
     digit_1000_num = random(9);
   }
-  
+
   // 1の桁
   digitalWrite(digit_10, 0);
   digitalWrite(digit_100, 0);
@@ -182,7 +152,7 @@ void dispRandom() {
   digitalWrite(digit_1, 1);
   numPrint(digit_1_num);
   delay(del);
-  
+
   // 10の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_100, 0);
@@ -190,7 +160,7 @@ void dispRandom() {
   digitalWrite(digit_10, 1);
   numPrint(digit_10_num);
   delay(del);
-  
+
   // 100の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_10, 0);
@@ -198,14 +168,33 @@ void dispRandom() {
   digitalWrite(digit_100, 1);
   numPrint(digit_100_num);
   delay(del);
-  
+
   // 1000の桁
   digitalWrite(digit_1, 0);
   digitalWrite(digit_10, 0);
   digitalWrite(digit_100, 0);
   digitalWrite(digit_1000, 1);
-  numPrint(digit_1000_num); 
+  numPrint(digit_1000_num);
   delay(del);
 
-  count = (count+1)%5;
+  count = (count + 1) % 5;
+}
+
+// 桁ごとのシリアル送信値から表示する数字への変換
+int convertNum(String s, int now_num, int c) {
+  int n = now_num;
+  // -はランダム表示
+  if (s == "-") {
+    // タイミングがあったときだけランダム値に
+    if (c == 0) {
+      n = random(9);
+    }
+  // それ以外は数字が来る
+  } else {
+    n = s.toInt();
+    if (n < 0 || n > 9) {
+      n = 0;
+    }
+  }
+  return n;
 }
